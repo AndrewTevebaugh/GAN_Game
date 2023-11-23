32f93011e23a43d49c02ebc7cf2a8b6d6a1dfd81
Andrew Tevebaugh <AndrewTevebaugh@gmail.com>
2023-11-23 00:31:42 -0500

    Added basic movement

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
 import pygame
-import time
 from src.game.game import Game
 # from src.gan.generate import generate_levels
 
 if __name__ == "__main__":
-    # Uncomment the following lines when you are ready to use the GAN-generated levels
-    # print("Generating levels with GAN...")
-    # generate_levels()
-    # print("Levels generated successfully.")
+  # Uncomment the following lines when you are ready to use the GAN-generated levels
+  # print("Generating levels with GAN...")
+  # generate_levels()
+  # print("Levels generated successfully.")
 
-    print("Running the game...")
+  print("Running the game...")
 
-    # Initialize Pygame
-    pygame.init()
+  # Initialize Pygame
+  pygame.init()
 
-    # Set up the game
-    game = Game()
-    # game.initialize()
+  # Set up the game
+  game = Game()
+  # game.initialize()
 
-    time.sleep(3)
+  # Run the game loop
+  while game.is_running:
+    game.handle_events()
+    game.update()
+    game.render()
 
-    # Run the game loop
-    # while game.is_running:
-    #     game.handle_events()
-    #     game.update()
-    #     game.render()
-
-    # Clean up Pygame
-    pygame.quit()
+  # Clean up Pygame
+  pygame.quit()
--- src/game/game.py
+++ src/game/game.py
@@ -1,13 +1,59 @@
 import pygame
 
-
 class Game:
-  def __init__(self, w=640, h=480):
-    self.w = w
-    self.h = h
+  def __init__(self):
+    self.w = 500
+    self.h = 500
+    self.playerX = 0
+    self.playerY = 0
+    self.playerW = 25
+    self.playerH = 25
+    self.playerVelocity = 0.2
     self.screen = pygame.display.set_mode((self.w, self.h))
     pygame.display.set_caption("GAN Game")
+    self.clock = pygame.time.Clock()
+    self.screen.fill("blue")
+    self.is_running = True
 
+  def handle_events(self):
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        self.is_running = False
 
+    self.keys = pygame.key.get_pressed()
 
-  #TODO Complete other functions
+    self.clock.tick(60)
+
+  def update(self):
+    self.position = 0
+
+    if(self.keys[pygame.K_w]):
+      self.playerY -= 25.0 * self.playerVelocity
+      # Very basic boundary check
+      if(self.playerY < 0):
+        self.playerY = 0
+
+    if(self.keys[pygame.K_s]):
+      self.playerY += 25.0 * self.playerVelocity
+      # Very basic boundary check
+      if(self.playerY > (self.h - self.playerH)):
+        self.playerY = (self.h - self.playerH)
+
+    if(self.keys[pygame.K_d]):
+      self.playerX += 25.0 * self.playerVelocity
+      # Very basic boundary check
+      if(self.playerX > (self.w - self.playerW)):
+        self.playerX = (self.w - self.playerW)
+
+    if(self.keys[pygame.K_a]):
+      self.playerX -= 25.0 * self.playerVelocity
+      # Very basic boundary check
+      if(self.playerX < 0):
+        self.playerX = 0
+
+  def render(self):
+    self.screen.fill("blue")
+    pygame.draw.rect(self.screen, "pink", (self.playerX, self.playerY, self.playerW, self.playerH))
+    # for tile in self.map:
+    #   pygame.draw.rect()
+    pygame.display.update()
