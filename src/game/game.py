eb9803e099f279ea02b5974a239f1df4d7a16388
Andrew Tevebaugh <AndrewTevebaugh@gmail.com>
2023-11-25 00:06:04 -0500

    Player starts at spawnpoint

--- src/game/game.py
+++ src/game/game.py
@@ -6,19 +6,18 @@ class Game:
   def __init__(self):
     self.w = 500
     self.h = 500
-    self.playerX = 0
-    self.playerY = 0
     self.playerW = 25
     self.playerH = 25
     self.playerVelocity = 0.2
     self.screen = pygame.display.set_mode((self.w, self.h))
     pygame.display.set_caption("GAN Game")
     self.clock = pygame.time.Clock()
-    #TODO Add a level loader
+    #TODO Add a configurable level loader
     self.tileList = np.loadtxt("src\\game\\levels\\testLevel.txt", dtype=int)
-    # self.map = np.zeros((20,20), dtype=int)
-    # for (row_idx, col_idx), tile in np.ndenumerate(tileList):
-    #   self.map[row_idx][col_idx] = tile 
+    for (row_idx, col_idx), tile in np.ndenumerate(self.tileList):
+      if(tile == lh.tileType.START):
+        self.playerX = col_idx*25
+        self.playerY = row_idx*25
     self.is_running = True
 
   def handle_events(self):
