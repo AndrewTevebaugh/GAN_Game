5f43911b4adcde7b19b0dc3cec2b014994a5dc54
AndrewTevebaugh <81001466+AndrewTevebaugh@users.noreply.github.com>
2023-11-27 22:34:40 -0500

    Fixed collision to be smooth, this took 3 hours so its ugly as shit

--- src/game/movement.py
+++ src/game/movement.py
@@ -34,12 +34,26 @@ def check_collisions(oldX, newX, oldY, newY, tileList):
 
   # Check for collisions with other tiles
   # TODO: currently super basic; planning on checking the direction player came from so you dont get stuck on the wall.
-  playerRect = pygame.Rect(newX, newY, 25, 25)
   for (row_idx, col_idx), tile in np.ndenumerate(tileList):
+    playerRect = pygame.Rect(newX, newY, 25, 25)
     tileRect = pygame.Rect(col_idx*25, row_idx*25, 25, 25)
     if(pygame.Rect.colliderect(playerRect, tileRect) and tile == lh.tileType.WALL):
-      newX = oldX
-      newY = oldY
+      fixX, fixY = 0, 0
+      for (row_idx2, col_idx2), tile2 in np.ndenumerate(tileList):
+        playerRect2X = pygame.Rect(oldX, newY, 25, 25)
+        playerRect2Y = pygame.Rect(newX, oldY, 25, 25)
+        tileRect2 = pygame.Rect(col_idx2*25, row_idx2*25, 25, 25)
+        if(pygame.Rect.colliderect(playerRect2X, tileRect2) and tile2 == lh.tileType.WALL):
+          fixX += 1
+        if(pygame.Rect.colliderect(playerRect2Y, tileRect2) and tile2 == lh.tileType.WALL):
+          fixY += 1
+      if(fixX == 0):
+        newX = oldX
+      elif(fixY == 0):
+        newY = oldY
+      else:
+        newX = oldX
+        newY = oldY
 
   return (newX, newY)
 
