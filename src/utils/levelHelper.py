088d2ae8d1c713869a14bbdf254bd44cd07bee15
AndrewTevebaugh <81001466+AndrewTevebaugh@users.noreply.github.com>
2023-12-14 23:36:08 -0500

    Reverted back to old values for tiles

--- src/utils/levelHelper.py
+++ src/utils/levelHelper.py
@@ -1,32 +1,37 @@
 from enum import IntEnum
 
-GRAY = (99,99,99)
-BROWN = (69, 58, 34)
-BLACK = (0, 0, 0)
-TEAL = (13, 158, 190)
-RED = (102, 6, 6)
-GOLD = (194, 193, 75)
-PURPLE = (86, 15, 148)
+WHITE = (0, 0, 0) # ERROR
+GRAY = (99,99,99) # Traversed
+BROWN = (69, 58, 34) # Open
+BLACK = (0, 0, 0) # Wall
+PURPLE = (86, 15, 148) # Start
+TEAL = (13, 158, 190) # Exit
+RED = (102, 6, 6) # Hazard
+GOLD = (194, 193, 75) # Coin
 
 class tileType(IntEnum):
-  OPEN = 10
-  TRAVERSED = -9
-  WALL = -10000
-  DOOR = 1000
-  HAZARD = -1000
-  COIN = 100
-  START = -100
+  WALL = 0
+  START = 1
+  HAZARD = 2
+  TRAVERSED = 4
+  OPEN = 6
+  COIN = 8
+  DOOR = 9
 
 def getTileColor(tile):
   if(tile == tileType.WALL):
     return BLACK
-  elif(tile == tileType.DOOR):
-    return TEAL
-  elif(tile == tileType.HAZARD):
-    return RED
-  elif(tile == tileType.COIN):
-    return GOLD
   elif(tile == tileType.START):
     return PURPLE
-  else:
+  elif(tile == tileType.HAZARD):
+    return RED
+  elif(tile == tileType.TRAVERSED):
+    return GRAY
+  elif(tile ==tileType.OPEN):
     return BROWN
+  elif(tile == tileType.COIN):
+    return GOLD
+  elif(tile == tileType.DOOR):
+    return TEAL
+  else:
+    return WHITE
